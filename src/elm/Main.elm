78e7474c5b1b1dca6e199f1141bb49aa39fecf9a
Jovanny Cruz <jovannypcg@yahoo.com>
2017-11-03 20:46:19 -0600

    Add app logic

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
 
 
 -- APP
@@ -51,12 +53,19 @@ init =
 
 type Msg
     = FindJoke
+    | NewJoke (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         FindJoke ->
+            ( model, getRandomJoke )
+
+        NewJoke (Ok joke) ->
+            ( { model | jokes = joke :: model.jokes }, Cmd.none )
+
+        _ ->
             Model [] ! []
 
 
@@ -68,6 +77,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ header
+        , jokeSection model.jokes
         ]
 
 
@@ -85,11 +95,51 @@ header =
         ]
 
 
-{-| Provides an Html divider
+{-| Displays text in form of a card by Materialize.
 -}
-divider : Html msg
-divider =
-    div [ class "row" ]
+toCard : String -> Html Msg
+toCard content =
+    div [ class "row container" ]
         [ div [ class "col s8 offset-s2" ]
-            [ div [ class "divider" ] [] ]
+            [ div [ class "card blue-grey darken-1" ]
+                [ div [ class "card-content white-text center" ]
+                    [ p [] [ text content ] ]
+                ]
+            ]
         ]
+
+
+{-| Provides section to show jokes saved in model.
+-}
+jokeSection : List String -> Html Msg
+jokeSection jokes =
+    section [] [ div [] (List.map toCard <| jokes) ]
+
+
+
+-- REQUESTS
+
+
+{-| Request a random joke from the Chuck Norris Database.
+-}
+getRandomJoke : Cmd Msg
+getRandomJoke =
+    let
+        url =
+            "https://api.icndb.com/jokes/random"
+
+        request =
+            Http.get url decodeJoke
+    in
+    Http.send NewJoke request
+
+
+{-| Given a response from the Chuck Norris Database, for instance:
+{ "value": { "id": 527, "joke": "No one has ever lived to tell about it." } }
+
+This function retrieves the value of "joke" attribute.
+
+-}
+decodeJoke : Decode.Decoder String
+decodeJoke =
+    Decode.at [ "value", "joke" ] Decode.string
